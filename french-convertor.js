96d22b97f089d56b73a180fff56e2988dcbd6b27
bradleyr87 <bradleydesigner@gmail.com>
2023-11-08 19:08:44 +0000

    Small tidy up

--- french-convertor.js
+++ french-convertor.js
@@ -6,18 +6,18 @@ const readLine = readline.createInterface({
     output: process.stdout
   });
 
-// TODO: Move all magic strings to top
+// TODO: Move all magic strings to conts, refactor  
 const units = ["zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize"];
 const tens = ["dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante-dix", "quatre-vingts", "quatre-vingt-dix"];
 
 function numbersToFrenchWords(number){
 
-    if(IsUnits(number)) return units[number]  
+    if(IsUnits(number)) return units[number];  
     //tens
     if(IsTens(number)) return TensBusinessLogic(number); 
     //hundreds
     if(IsHundreds(number)) return HundredsBusinessLogic(number);
-    //thousand
+    //thousands
     if(IsThousandsOrGreater(number)) return ThousandsBusinessLogic(number);
 }
 
@@ -46,28 +46,28 @@ function TensLessThan70(number){
   let unit = number % 10;
   if(unit === 0){
     return tens[ten - 1]
-  }else if(number % 10 == 1 && number != 81 || number % 10 == 1 && number != 91 ){
-    return `${tens[ten - 1]}-et-${units[unit]}`
+  }
+  else if(number % 10 == 1 && number != 81 || number % 10 == 1 && number != 91 ){
+    return `${tens[ten - 1]}-et-${units[unit]}`;
   }
   else{
-    return `${tens[ten - 1]}-${units[unit]}` 
+    return `${tens[ten - 1]}-${units[unit]}`; 
   }
 }
 
 function HundredsBusinessLogic(number){
-  if(number == 100) return 'cent'
+  if(number == 100) return 'cent';
   if(number < 200) return `cent-${numbersToFrenchWords(number % 100)}`;
-  if(number % 100 === 0) return `${units[Math.floor(number / 100)]}-cents`
+  if(number % 100 === 0) return `${units[Math.floor(number / 100)]}-cents`;
   return numbersToFrenchWords(Math.floor(number / 100)) + "-cents-" + numbersToFrenchWords(number % 100);
 }
 
 function ThousandsBusinessLogic(number){
-  if(number == 1000) return 'mille'
-  if(number < 2000) return `mille-${numbersToFrenchWords(number % 100)}`;
-  if(number % 1000 === 0) return `${units[Math.floor(number / 1000)]}-miles`
+  if(number == 1000) return 'mille';
+  if(number < 2000) return `mille-${numbersToFrenchWords(number % 1000)}`;
+  if(number % 1000 === 0) return `${units[Math.floor(number / 1000)]}-miles`;
   return numbersToFrenchWords(Math.floor(number / 1000)) + "-miles-" + numbersToFrenchWords(number % 1000);
 }
-
 
 function IsHundreds(number){
   return number >= 100 && number < 1000;
@@ -77,21 +77,19 @@ function IsThousandsOrGreater(number){
   return number >= 1000;
 }
 
-
 function IsValid(input){
   return input !== '';
 }
 
 readLine.question('Enter a comma seperated list: ', (input) => {
     if(!IsValid(input)){
-      console.error('Error: comma seperated list not provided or invalid'); 
+      error('Error: comma seperated list not provided or invalid'); 
       return readLine.close();
     }
     
     const numbers = input.trim().split(',').map(Number);
-    console.log('input numbers: ', numbers);
     const result = numbers.map(numbersToFrenchWords);
-    console.log("Numbers to French words: ", result.join(', '));
+    log("Numbers to French words: ", result.join(', '));
     readLine.close();
   });
   
